3a30b9f19111923b1e48b26dc75f50c150fd5363
NatJerry <xiaomajunqwq123@outlook.com>
2023-01-02 17:57:59 +0800

    Init Plugin

---
+++ src/main/java/org/ncc/github/chatwithgroup/ChatWithGroup.java
@@ -0,0 +1,17 @@
+package org.ncc.github.chatwithgroup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatWithGroup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
